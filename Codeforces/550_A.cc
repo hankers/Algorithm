2ffab2fd1bb9326d4f5a09d68960229b0a1267db
root <root@localhost.localdomain>
2015-06-08 20:35:54 +0800

    add 550_A

---
+++ Codeforces/550_A.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	bool flag = false;
+	int len;
+	string str;
+	vector<int> vec1, vec2;
+	while (cin >> str) {
+		flag = false;
+		vec1.clear();
+		vec2.clear();
+		len = str.length();
+
+		for (int i = 0; i < len-1; i ++) {
+			if (str[i] == 'A' && str[i+1] == 'B')
+				vec1.push_back(i);
+			if (str[i] == 'B' && str[i+1] == 'A')
+				vec2.push_back(i);
+		}
+
+		for (int i = 0; i < vec1.size(); i ++) {
+			for (int j = 0; j < vec2.size(); j ++) {
+				if (vec1[i] != vec2[j]-1 && vec1[i] != vec2[j]+1) {
+					cout << "YES" << endl;
+					return 0;
+				}
+			}
+		
+		}
+		cout << "NO" << endl;
+	}
+
+	return 0;
+}
